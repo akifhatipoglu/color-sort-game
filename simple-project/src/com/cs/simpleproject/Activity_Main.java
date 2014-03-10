0e0cc16052cdfe375eb2183f28effd07c1854e06
akifhatipoglu <akifhatipoglu.tech@gmail.com>
2014-03-10 23:18:53 +0200

    Main menu was added.

---
+++ simple-project/src/com/cs/simpleproject/Activity_Main.java
@@ -0,0 +1,14 @@
+package com.cs.simpleproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_Main extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+	}
+}
