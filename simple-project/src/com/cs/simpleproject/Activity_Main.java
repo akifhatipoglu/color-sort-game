4ed4d331c1740fd8e3408421b8dae277d9b20832
akifhatipoglu <akifhatipoglu.tech@gmail.com>
2014-03-10 23:23:04 +0200

    Exit button is activated.

--- simple-project/src/com/cs/simpleproject/Activity_Main.java
+++ simple-project/src/com/cs/simpleproject/Activity_Main.java
@@ -2,6 +2,8 @@ package com.cs.simpleproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Main extends Activity {
 
@@ -9,6 +11,14 @@ public class Activity_Main extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		Button button_exit=(Button) findViewById(R.id.button_Exit);
+		button_exit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 
 	}
 }
