b5530f762794d022d70ed187a24450efa2805991
akifhatipoglu <akifhatipoglu.tech@gmail.com>
2014-03-11 00:30:01 +0200

    Alert Dialog (Radio button) is added but not activated.

--- simple-project/src/com/cs/simpleproject/Activity_NewGame.java
+++ simple-project/src/com/cs/simpleproject/Activity_NewGame.java
@@ -1,17 +1,117 @@
 package com.cs.simpleproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Activity_NewGame extends Activity{
+	private static final int LEVEL_EASY=0;
+	private static final int LEVEL_MEDIUM=1;
+	private static final int LEVEL_HARD=3;
+	private static final int DIALOG=0;
+	
+	final Context cont=getApplicationContext();
 	
 	@Override
  	protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.newgame);
 	
+	Button button_easy=(Button) findViewById(R.id.button_Easy);
+	Button button_medium=(Button) findViewById(R.id.button_Medium);
+	Button button_hard=(Button) findViewById(R.id.button_Hard);
+	
+	button_easy.setOnClickListener(new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			onCreateDialog(LEVEL_EASY);
+			Toast.makeText(cont, "as", Toast.LENGTH_LONG).show();
+		}
+	});
+	
+	button_medium.setOnClickListener(new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			onCreateDialog(LEVEL_MEDIUM);	
+		}
+	});
+	
+	button_hard.setOnClickListener(new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			onCreateDialog(LEVEL_HARD);	
+		}
+	});
 	}
+	
+	protected Dialog getChooseItemCheckedDialog(final int level) {
+		final CharSequence[] items={"Section 1","Section 2","Section 3","Section 4"};
+		
+		AlertDialog.Builder builder=new AlertDialog.Builder(this);
+		builder.setTitle("Choose section");
+		builder.setSingleChoiceItems(items, 0,new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(cont, items[0], Toast.LENGTH_LONG).show();
+				if(level==LEVEL_EASY){}
+				if(level==LEVEL_MEDIUM){}
+				if(level==LEVEL_HARD){}
+				dialog.dismiss();
+			}
+		});
+		builder.setSingleChoiceItems(items, 1,new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(cont, items[0], Toast.LENGTH_LONG).show();
+				if(level==LEVEL_EASY){}
+				if(level==LEVEL_MEDIUM){}
+				if(level==LEVEL_HARD){}
+				dialog.dismiss();
+			}
+		});
+		builder.setSingleChoiceItems(items, 2,new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(cont, items[0], Toast.LENGTH_LONG).show();
+				if(level==LEVEL_EASY){}
+				if(level==LEVEL_MEDIUM){}
+				if(level==LEVEL_HARD){}
+				dialog.dismiss();
+			}
+		});
+		builder.setSingleChoiceItems(items, 3,new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(cont, items[0], Toast.LENGTH_LONG).show();
+				if(level==LEVEL_EASY){}
+				if(level==LEVEL_MEDIUM){}
+				if(level==LEVEL_HARD){}
+				dialog.dismiss();
+			}
+		});
+		
+		return builder.create();
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog =getChooseItemCheckedDialog(id);
+		
+		return dialog;
+	}
+	
 	@Override
 	public void onBackPressed() {
 		Intent setIntent =new  Intent(this, Activity_Main.class);
